2c3174ae52033ce7a6ccfab9bb3a1e38048526cf
Aquib Shaikh <shaikhaquib474@gmail.com>
2026-04-22 10:50:35 +0530

    Fixed the domain to brionest.in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/Home"
 function App() {
 	return (
 		<div className="App">
-			<BrowserRouter basename="/brionest">
+			<BrowserRouter>
 				<Routes>
 					<Route path="/" element={<Home />} />
 				</Routes>
